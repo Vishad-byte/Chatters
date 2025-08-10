9a51b82da864f5ab4f75117889ae9080c96641db
Vishad-byte <srivastavavishad0212@gmail.com>
2025-08-10 15:47:27 +0530

    Add useEffect import to ProfilePage for future enhancements

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 import { Camera, Mail, User } from 'lucide-react';
 
